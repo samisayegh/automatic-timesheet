3d4ce4dcd02d184d179dc7eeecbb726381ade32d
Sami Sayegh <ssayegh@coveo.com>
2020-10-27 13:24:44 -0400

    add utc date param

--- src/jira-client/jira-client.test.ts
+++ src/jira-client/jira-client.test.ts
@@ -7,15 +7,22 @@ jest.mock('axios');
 describe('Jira Client', () => {
   it('#logTime sends a request with the correct params', async () => {
     const client = new JiraClient(axios);
-
+    
+    const october27th = new Date()
+    october27th.setUTCFullYear(2020, 10, 27);
+    
     await client.logTime({
       issueId: 'KIT-123',
       workLogId: 1,
-      hours: 2
+      hours: 2,
+      utc: october27th
     })
 
     const url = 'https://coveord.atlassian.net/rest/api/2/issue/KIT-123/worklog?adjustEstimate=auto&_r=1'
-    const data = {timeSpent: '2h'}
+    const data = {
+      timeSpent: '2h',
+      started: '2020-10-27T09:00:00.000+0000'
+    }
 
     expect(axios.post).toHaveBeenCalledTimes(1);
     expect(axios.post).toHaveBeenCalledWith(url, data);
--- src/jira-client/jira-client.ts
+++ src/jira-client/jira-client.ts
@@ -4,19 +4,30 @@ interface LogTimeProps {
   issueId: string;
   workLogId: number;
   hours: number;
+  utc: Date;
 }
 
 export class JiraClient {
   constructor(private http: AxiosStatic) {}
 
   public async logTime(options: LogTimeProps) {
-    const {issueId, workLogId, hours} = options;
+    const {issueId, workLogId, hours, utc} = options;
     
     const url = `https://coveord.atlassian.net/rest/api/2/issue/${issueId}/worklog?adjustEstimate=auto&_r=${workLogId}`
+
     const data = {
+      started: buildDateString(utc),
       timeSpent: `${hours}h`
     }
 
     await this.http.post(url, data);
   }
+}
+
+function buildDateString(date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  return `${year}-${month}-${day}T09:00:00.000+0000`
 }
