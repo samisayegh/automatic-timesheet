557907540be3f3e3b83b389b2f0eb74a41c31d04
Sami Sayegh <ssayegh@coveo.com>
2020-10-28 13:10:08 -0400

    restore jira-cred resolver

--- src/security/jira-credentials-resolver.ts
+++ src/security/jira-credentials-resolver.ts
@@ -5,9 +5,9 @@ export interface ICredentialsResolver {
 
 export class JiraCredentialsResolver implements ICredentialsResolver {
     getEmail(): string {
-        return process.env.TIMESHEET_JIRA_EMAIL || 'ssayegh@coveo.com';
+        return process.env.TIMESHEET_JIRA_EMAIL || '';
     }
     getApiToken() : string {
-        return process.env.TIMESHEET_JIRA_API_KEY || 'tRAKKmfbDGvJxK2gdi2KE3B2';
+        return process.env.TIMESHEET_JIRA_API_KEY || '';
     }
 }
