b7bb1e61c3fc2c6cf75bb2bf2fd8fb785dd64c3a
Francisco Guerreiro <fguerreiro@coveo.com>
2020-10-28 11:02:01 -0400

    adds log calculator service

---
+++ src/services/log-calculator.ts
@@ -0,0 +1,18 @@
+
+export interface CommitInfo {
+    author: string,
+    id: string,
+    timestamp: Date
+}
+
+/**
+ *  
+ */
+function calculateFromCommits(logDate: Date, commitInfos: CommitInfo[]) { 
+
+    // iterate through commits in CommitInfo[]
+    // for each commit c in CommitInfo
+    // if it is in dateToLog, add to ListLog.
+
+    // Calculate all records in ListLog, take the first two, and divide time of 8.
+}
