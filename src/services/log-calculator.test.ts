5513200b9fd04f11721b9c2d026915a86596bc44
Francisco Guerreiro <fguerreiro@coveo.com>
2020-10-28 15:38:35 -0400

    Added test and implementation for two different issues same day

--- src/services/log-calculator.test.ts
+++ src/services/log-calculator.test.ts
@@ -43,4 +43,38 @@ describe('log calculator', () => {
     expect(result).toBeDefined();
     expect(result.logCommands[0].issueKey).toBe(expectedIssueKey);
   })
+
+  it('#calculateFromCommits should produce two log commands', () => {
+    const expectedIssueKey1 = 'SFCT-4242';
+    const expectedIssueKey2 = 'SFCT-4343';
+
+    const commit2 = {
+        id: '1234',
+        author: {
+            name: 'fguerreiro'
+        },
+        timestamp: `${dateOfCommitText}T02:12:38Z`
+    }
+
+    const commitsForIssue1: CommitsForIssue = {
+        commits: [commit1],
+        issueKey: expectedIssueKey1
+    }
+
+    const commitsForIssue2: CommitsForIssue = {
+        commits: [commit2],
+        issueKey: expectedIssueKey2
+    }
+
+    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [commitsForIssue1, commitsForIssue2]);
+
+    expect(result).toBeDefined();
+    expect(result.logCommands).toHaveLength(2);
+    // expect(result.logCommands[0].issueKey).toBe(expectedIssueKey1);  
+    // expect(result.logCommands[1].issueKey).toBe(expectedIssueKey2);
+  })
+
+  // todo: should produce two log commands with separate issues
+  // should produce 2 log commands for the same day
+  // should not produce log commands outside the date range
 });
