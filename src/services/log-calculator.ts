b40691f3d75f8e76cc51dfa04f102254e3f90d5e
Francisco Guerreiro <fguerreiro@coveo.com>
2020-10-28 11:06:50 -0400

    adds LogCommand interface and return type, and TimeToLog enum

--- src/services/log-calculator.ts
+++ src/services/log-calculator.ts
@@ -5,14 +5,26 @@ export interface CommitInfo {
     timestamp: Date
 }
 
+export interface LogCommand {
+    issueKey: string,
+    logTime: TimeToLog
+}
+
+// Enum to restrict business rule by type: should be either four or eight hours, nothing outside of that.
+enum TimeToLog {
+    four,
+    eight
+}
+
 /**
  *  
  */
-function calculateFromCommits(logDate: Date, commitInfos: CommitInfo[]) { 
+function calculateFromCommits(logDate: Date, commitInfos: CommitInfo[]) : LogCommand[] { 
 
     // iterate through commits in CommitInfo[]
     // for each commit c in CommitInfo
     // if it is in dateToLog, add to ListLog.
 
     // Calculate all records in ListLog, take the first two, and divide time of 8.
+    return [];
 }
