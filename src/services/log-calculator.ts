c0081868e96f325158357570244c8be96ba58846
Francisco Guerreiro <fguerreiro@coveo.com>
2020-10-28 11:11:27 -0400

    moves function to class LogCalculator

--- src/services/log-calculator.ts
+++ src/services/log-calculator.ts
@@ -16,16 +16,15 @@ enum TimeToLog {
     eight
 }
 
-/**
- *  
- */
-// @ts-ignore
-function calculateFromCommits(logDate: Date, commitInfos: CommitInfo[]) : LogCommand[] { 
+export class LogCalculator {
+    // @ts-ignore
+    public calculateFromCommits(logDate: Date, commitInfos: CommitInfo[]) : LogCommand[] { 
 
-    // iterate through commits in CommitInfo[]
-    // for each commit c in CommitInfo
-    // if it is in dateToLog, add to ListLog.
+        // iterate through commits in CommitInfo[]
+        // for each commit c in CommitInfo
+        // if it is in dateToLog, add to ListLog.
 
-    // Calculate all records in ListLog, take the first two, and divide time of 8.
-    return [];
+        // Calculate all records in ListLog, take the first two, and divide time of 8.
+        return [];
+    }
 }
