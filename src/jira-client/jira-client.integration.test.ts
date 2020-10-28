f02815be5ec62df639961e9259d94003afe67d43
Francisco Guerreiro <fguerreiro@coveo.com>
2020-10-28 10:40:13 -0400

    Adapts getIssuesInProgress to have input of date range. Adds formatDate utils function.

--- src/jira-client/jira-client.integration.test.ts
+++ src/jira-client/jira-client.integration.test.ts
@@ -4,12 +4,16 @@ import { JiraCredentialsResolver } from '../security/jira-credentials-resolver';
 
 it('#getIssuesInProgress returns a valid response with issues', async () => {
   const client = new JiraClient(axios, new JiraCredentialsResolver());
-  const result = await client.getIssuesInProgress();
+  const result = await client.getIssuesInProgress('2020-10-27', '2020-10-27');
 
   expect(result.data).toMatchObject({
     issues: []
   })
 })
+
+// it('gets graphql results', async () => {
+//   await client.getDevDetailsForIssue();
+// })  
 
 // it('#getUsers return users', async () => {
 //   const client = new JiraClient(axios, new JiraCredentialsResolver());
