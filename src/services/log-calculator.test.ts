3c4fbcb7c0a5c8a53d13d431245cdaaf93fa06e6
Sami Sayegh <samisayegh1@gmail.com>
2020-10-31 14:48:22 -0400

    build UT data using a function

--- src/services/log-calculator.test.ts
+++ src/services/log-calculator.test.ts
@@ -4,42 +4,42 @@ import { IssueInfo } from '../models/timesheet-models';
 
 describe('log calculator', () => {
 
- let logCalculator: LogCalculator;
- let commit1: Commit;
- let dateOfCommitText: string;
+  let logCalculator: LogCalculator;
+  let commit1: Commit;
+  let dateOfCommitText: string;
+
+  function buildCommit(config: Partial<Commit> = {}): Commit {
+    return {
+      author: {
+        name: ''
+      },
+      id: '',
+      timestamp: '',
+      ...config
+    }
+  }
+
+  function buildIssueInfo(config: Partial<IssueInfo> = {}): IssueInfo {
+    return {
+      issueKey: '',
+      commits: [],
+      ...config
+    }
+  }
 
   beforeAll(() => {
     logCalculator = new LogCalculator();
     dateOfCommitText = '2020-10-21';
-    commit1 = {
-        id: '123',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${dateOfCommitText}T00:12:38Z`
-    }
+
+    commit1 = buildCommit({timestamp: `${dateOfCommitText}T00:12:38Z`})
   })
-
-  it('#calculateFromCommits should produce a logCommand', () => {
-    const commitsForIssue: IssueInfo = {
-        commits: [commit1],
-        issueKey: 'SFCT-4242'
-    }
-    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [commitsForIssue]);
-
-    expect(result).toBeDefined();
-    expect(result.logCommands).toHaveLength(1);
-  })
-
+  
   it('#calculateFromCommits should have proper Issue key Id', () => {
     const expectedIssueKey = 'SFCT-4242';
-    const commitsForIssue: IssueInfo = {
-        commits: [commit1],
-        issueKey: expectedIssueKey
-    }
-    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [commitsForIssue]);
+    const issueInfo = buildIssueInfo({ issueKey: expectedIssueKey, commits: [commit1]})
+    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [issueInfo]);
 
-    expect(result).toBeDefined();
+    expect(result.logCommands).toHaveLength(1);
     expect(result.logCommands[0].issueKey).toBe(expectedIssueKey);
   })
 
@@ -47,29 +47,14 @@ describe('log calculator', () => {
     const expectedIssueKey1 = 'SFCT-4242';
     const expectedIssueKey2 = 'SFCT-4343';
 
-    const commit2 = {
-        id: '1234',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${dateOfCommitText}T02:12:38Z`
-    }
+    const commit2 = buildCommit({timestamp: `${dateOfCommitText}T02:12:38Z`});
+    const issueInfo1 = buildIssueInfo({commits: [commit1], issueKey: expectedIssueKey1});
+    const issueInfo2 = buildIssueInfo({commits: [commit2], issueKey: expectedIssueKey2});
+    
+    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [issueInfo1, issueInfo2]);
 
-    const commitsForIssue1: IssueInfo = {
-        commits: [commit1],
-        issueKey: expectedIssueKey1
-    }
-
-    const commitsForIssue2: IssueInfo = {
-        commits: [commit2],
-        issueKey: expectedIssueKey2
-    }
-
-    const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [commitsForIssue1, commitsForIssue2]);
-
-    expect(result).toBeDefined();
     expect(result.logCommands).toHaveLength(2);
-    expect(result.logCommands[0].issueKey).toBe(expectedIssueKey1);  
+    expect(result.logCommands[0].issueKey).toBe(expectedIssueKey1);
     expect(result.logCommands[1].issueKey).toBe(expectedIssueKey2);
   })
 
@@ -77,34 +62,16 @@ describe('log calculator', () => {
     const expectedIssueKey1 = 'SFCT-4242';
     const expectedIssueKey2 = 'SFCT-4343';
 
-    const commit2 = {
-        id: '1234',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${dateOfCommitText}T02:12:38Z`
-    }
+    const commit2 = buildCommit({ timestamp: `${dateOfCommitText}T02:12:38Z`});
 
-    const commitsForIssue1: IssueInfo = {
-        commits: [commit1],
-        issueKey: expectedIssueKey1
-    }
-
-    const commitsForIssue2: IssueInfo = {
-        commits: [commit2],
-        issueKey: expectedIssueKey2
-    }
-
-    const commitsForIssue3: IssueInfo = {
-        commits: [commit2],
-        issueKey: 'SFCT-0001'
-    }
+    const commitsForIssue1 = buildIssueInfo({ commits: [commit1], issueKey: expectedIssueKey1});
+    const commitsForIssue2 = buildIssueInfo({ commits: [commit2], issueKey: expectedIssueKey2 });
+    const commitsForIssue3 = buildIssueInfo({ commits: [commit2], issueKey: 'SFCT-0001'});
 
     const result = logCalculator.calculateFromCommits(new Date(dateOfCommitText), [commitsForIssue1, commitsForIssue2, commitsForIssue3]);
 
-    expect(result).toBeDefined();
     expect(result.logCommands).toHaveLength(2);
-    expect(result.logCommands[0].issueKey).toBe(expectedIssueKey1);  
+    expect(result.logCommands[0].issueKey).toBe(expectedIssueKey1);
     expect(result.logCommands[1].issueKey).toBe(expectedIssueKey2);
   })
 
@@ -113,27 +80,12 @@ describe('log calculator', () => {
     const expectedIssueKey2 = 'SFCT-4343';
     const differentDate = '2020-01-01';
 
-    const commit2 = {
-        id: '1234',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${dateOfCommitText}T02:12:38Z`
-    }
-
-    const commitsForIssue1: IssueInfo = {
-        commits: [commit1],
-        issueKey: expectedIssueKey1
-    }
-
-    const commitsForIssue2: IssueInfo = {
-        commits: [commit2],
-        issueKey: expectedIssueKey2
-    }
-
+    const commit2 = buildCommit({ timestamp: `${dateOfCommitText}T02:12:38Z`});
+    const commitsForIssue1 = buildIssueInfo({commits: [commit1], issueKey: expectedIssueKey1})
+    const commitsForIssue2 = buildIssueInfo({commits: [commit2], issueKey: expectedIssueKey2})
+    
     const result = logCalculator.calculateFromCommits(new Date(differentDate), [commitsForIssue1, commitsForIssue2]);
 
-    expect(result).toBeDefined();
     expect(result.logCommands).toHaveLength(0);
   })
 
@@ -144,32 +96,12 @@ describe('log calculator', () => {
     const date2 = '2020-01-02';
     const dates = [new Date(date1), new Date(date2)];
 
-    const commit1 = {
-        id: '122',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${date1}T01:12:38Z`
-    }
+    const commit1 = buildCommit({timestamp: `${date1}T01:12:38Z`});
+    const commit2 = buildCommit({timestamp: `${date2}T02:12:38Z`});
 
-    const commit2 = {
-        id: '1234',
-        author: {
-            name: 'fguerreiro'
-        },
-        timestamp: `${date2}T02:12:38Z`
-    }
-
-    const commitsForIssue1: IssueInfo = {
-        commits: [commit1],
-        issueKey: expectedIssueKey1
-    }
-
-    const commitsForIssue2: IssueInfo = {
-        commits: [commit2],
-        issueKey: expectedIssueKey2
-    }
-
+    const commitsForIssue1 = buildIssueInfo({commits: [commit1], issueKey: expectedIssueKey1})
+    const commitsForIssue2 = buildIssueInfo({commits: [commit2], issueKey: expectedIssueKey2})
+    
     const result = logCalculator.calculateFromCommitsRetroactive(dates, [commitsForIssue1, commitsForIssue2]);
 
     expect(result).toBeDefined();
